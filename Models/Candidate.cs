87a291599d9322449b85393b881f82f735853d66
buihongson6203 <buihongson6203@gmail.com>
2024-05-07 09:51:34 +0700

    update database

---
+++ Models/Candidate.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BERecruitmentss.Models
+{
+    public class Candidate :Base
+    {
+        public int CandidateCode { get; set; }
+        public string Name { get; set;}
+        public int Status { get; set; }
+       
+    }
+}
