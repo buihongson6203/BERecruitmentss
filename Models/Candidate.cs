df587762012415544484bf0674916a279f2cc3a8
phuongthao <nguyenphuongthao22012004@gmail.com>
2025-04-11 23:57:21 +0700

    git add

--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -5,7 +5,6 @@ namespace BERecruitmentss.Models
 {
     public class Candidate :Base
     {
-        public string? CandidateCode { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
